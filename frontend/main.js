6c587707ac34f4a5c659b920dd7e23037ff23ddf
Jonalogy <jonalogy@gmail.com>
2016-09-13 20:21:10 +0800

    Created MVP for adding new score entry.

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   console.log('DOM content loaded!')
 
+  //#newEntry is the id of the submit button.
   $('#newEntry').submit(function(event){
     event.preventDefault();
     var newData = $('#newEntry').serialize();
@@ -9,9 +10,7 @@ $(document).ready(function(){
     addNewScore(newData);
   })
 
-
   listAll();
-
 }); //End DOM Content Loaded
 
 
@@ -21,7 +20,6 @@ function listAll(){
   $.get('http://localhost:3000/entries',function(data){
 
     for(i=0; i<data.length; i++){
-
       var idCreate = $('<p>').text(data[i]['id']);
       var idHolder = idCreate.appendTo($('<div>'));
       idHolder.appendTo($('#idCol'));
@@ -42,7 +40,22 @@ function addNewScore(newDataSend){
     url: 'http://localhost:3000/entries',
     type: 'POST',
     data: newDataSend
-  }).done(function(data){
-    listAll();
+  }).done(function(server_data){
+    listNewEntry(server_data);
     }
   )}
+
+function listNewEntry(data){
+  console.log(data);
+  var idCreate = $('<p>').text(data.id);
+  var idHolder = idCreate.appendTo($('<div>'));
+  idHolder.appendTo($('#idCol'));
+
+  var nameNew = $('<p>').text(data.name);
+  var nameHolder = nameNew.appendTo($('<div>'));
+  nameHolder.appendTo($('#nameCol'));
+
+  var scoreNew = $('<p>').text(data.score);
+  var scoreHolder = scoreNew.appendTo($('<div>'));
+  scoreHolder.appendTo($('#scoreCol'));
+}//END of listNewEntry()
