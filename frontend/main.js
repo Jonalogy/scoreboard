89ac53b162297434636c0636a7a4686a2786befb
Jonalogy <jonalogy@gmail.com>
2016-09-13 22:20:06 +0800

    Added Edit and Commit buttons

--- frontend/main.js
+++ frontend/main.js
@@ -11,68 +11,51 @@ $(document).ready(function(){
   })
 
   listAll();
+
 }); //End DOM Content Loaded
 
 
-////------Functions-------////
-
+////------Function Hoisting-------////
 function listAll(){
   $.get('http://localhost:3000/entries',function(data){
 
-    // for(i=0; i<data.length; i++){
-    //   var idCreate = $('<p>').text(data[i]['id']);
-    //   var idHolder = idCreate.appendTo($('<div>').attr('class','cell'));
-    //   idHolder.appendTo($('#idCol'));
-    //
-    //   var nameNew = $('<p>').text(data[i].name);
-    //   var nameHolder = nameNew.appendTo($('<div>'));
-    //   nameHolder.appendTo($('#nameCol'));
-    //
-    //   var scoreNew = $('<p>').text(data[i].score);
-    //   var scoreHolder = scoreNew.appendTo($('<div>'));
-    //   scoreHolder.appendTo($('#scoreCol'));
-    //
-    //   $('<button>').attr('type','button').attr('id','id'+data[i]['id']).attr('class','entryCtrl').text('Edit').appendTo($('#controlCol'));
-    // }
+      for(i=0; i<data.length; i++){
+        var rowNew = $('<div>').attr('id','row'+data[i]['id']).attr('class','row');
+        var idCreate = $('<div>').text(data[i]['id']).attr('class','cell');
+        var nameNew = $('<div>').text(data[i].name).attr('class','cell');
+        var scoreNew = $('<div>').text(data[i].score).attr('class','cell');
 
-    for(i=0; i<data.length; i++){
-      var rowNew = $('<div>').attr('id','row'+data[i]['id']).attr('class','row');
+        rowNew.append(idCreate).append(nameNew).append(scoreNew);
+        rowNew.appendTo($('#board'));
 
-      var idCreate = $('<div>').text(data[i]['id']).attr('class','cell');
-
-      var nameNew = $('<div>').text(data[i].name).attr('class','cell');
-
-      var scoreNew = $('<div>').text(data[i].score).attr('class','cell');
-
-      rowNew.append(idCreate).append(nameNew).append(scoreNew);
-      rowNew.appendTo($('#board'));
-    }
-
+        $('<button>').attr('type','button').attr('id','editButt'+data[i].id).text('Edit').appendTo(rowNew);
+        $('<button>').attr('type','button').attr('id','deleteButt'+data[i].id).text('Delete').appendTo(rowNew);
+      }// End of for loop
 
     });//End of GET route for /entries
-}
+}// End of listAll()
 
 function addNewScore(newDataSend){
   $.ajax({
     url: 'http://localhost:3000/entries',
     type: 'POST',
     data: newDataSend
-  }).done(function(server_data){
+    }).done(function(server_data){
     listNewEntry(server_data);
-    }
-  )}
+    })
 
-function listNewEntry(data){
-  console.log(data);
-  var idCreate = $('<p>').text(data.id);
-  var idHolder = idCreate.appendTo($('<div>'));
-  idHolder.appendTo($('#idCol'));
 
-  var nameNew = $('<p>').text(data.name);
-  var nameHolder = nameNew.appendTo($('<div>'));
-  nameHolder.appendTo($('#nameCol'));
+  function listNewEntry(data){
+    var rowNew = $('<div>').attr('id','row'+data.id).attr('class','row');
+    var idCreate = $('<div>').text(data.id).attr('class','cell');
+    var nameNew = $('<div>').text(data.name).attr('class','cell');
+    var scoreNew = $('<div>').text(data.score).attr('class','cell');
 
-  var scoreNew = $('<p>').text(data.score);
-  var scoreHolder = scoreNew.appendTo($('<div>'));
-  scoreHolder.appendTo($('#scoreCol'));
-}//END of listNewEntry()
+    rowNew.append(idCreate).append(nameNew).append(scoreNew);
+    rowNew.appendTo($('#board'));
+
+    $('<button>').attr('type','button').attr('id','editButt'+data.id).text('Edit').appendTo(rowNew);
+    $('<button>').attr('type','button').attr('id','deleteButt'+data.id).text('Delete').appendTo(rowNew);
+  }//END of listNewEntry()
+
+}//END of addNewScore()
